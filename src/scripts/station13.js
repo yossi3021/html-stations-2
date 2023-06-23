021e9aa4d1a1f2b4625d9f95a11044034d405c4e
yossi3021 <unitamilucky777@gmail.com>
2023-06-24 08:16:10 +0900

    すべてクリア

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,8 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  if(document.getElementById("check").checked){
+    document.getElementById('text').style.backgroundColor = 'red';
+  }else{
+    document.getElementById('text').style.backgroundColor = 'white';
+  }
 }
---
+++ src/scripts/station15.js
@@ -0,0 +1,5 @@
+//const fruits=document.querySelector('#fruits');
+function displayList(){
+    const fruits=document.querySelector('#fruits');
+    fruits.innerHTML='<ul><li>リンゴ</li><li>イチゴ</li><li>ブドウ</li></ul>'
+}
